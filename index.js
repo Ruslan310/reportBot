733d54e993059b5735d6bdfff15fe0a06c7cc088
“Klyzub <flash310xxx@gmail.com>
2024-08-02 09:28:35 -0700

    update 0.1

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-const TelegramBot= require("node-telegram-bot-api");
+const TelegramBot = require("node-telegram-bot-api");
 const axios = require('axios');
-// const token = '6460557426:AAGxWVU6WM8BG7FhOjTwVRqPH0zrUrQpaMU'; // order
-// const token = '6757361705:AAFy0xNuIn9E_NqhZa0vmhi2qsi-_wDPB8k'; // report
-const token = '6360641846:AAHosBn6WHKLWhALXdtYltR25sQrLbbgrKE'; // test
-const bot = new TelegramBot(token, {polling: true, filepath: false});
-const test_chat_id = '-4203108283'
 
-const botApi = 'https://api.telegram.org/bot6460557426:AAGxWVU6WM8BG7FhOjTwVRqPH0zrUrQpaMU'
-
-// const chatIdIndividual = '658137109'; // my group
-const chatId_reportGroup = '-4248491424'
+const token = '6460557426:AAGxWVU6WM8BG7FhOjTwVRqPH0zrUrQpaMU';
+const bot = new TelegramBot(token, { polling: true });
+const urlAmazon = 'https://0gb33gj0pj.execute-api.us-east-1.amazonaws.com/telegramBotGetMessage';
 
 bot.on('message', async msg => {
-  console.log('------msg.text', msg.text)
-  await bot.sendMessage(test_chat_id  , msg.text).catch(e => console.log(e));
-
+  const {text, chat} = msg;
 
   try {
-    // const result = await axios({
-    //   url: 'https://0gb33gj0pj.execute-api.us-east-1.amazonaws.com/telegramBotGetMessage',
-    //   method: 'post',
-    //   data: {
-    //     message: {
-    //       chat: {
-    //         title: 'Dinenation_report_Remonovo_',
-    //       },
-    //       text: '/Remonovo, 29-07-2024',
-    //     }
-    //   },
-    // });
-    // console.log('-----result', result)
+    await axios({
+      url: urlAmazon,
+      method: 'post',
+      data: {
+        message: {
+          chat: {
+            id: chat.id,
+          },
+          text: text,
+        }
+      },
+    });
   } catch (e) {
-    console.log('---------e', e.message)
+    console.error('Error:', e.message);
   }
-})
+});
