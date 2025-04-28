6b31ca0acfe78252c8ab3d3a2588a4ea9c43c9bd
Руслан Клызуб <ruslanklyzub@Mac.ht.home>
2025-04-27 19:50:43 -0700

    change api

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 const token = '6460557426:AAGxWVU6WM8BG7FhOjTwVRqPH0zrUrQpaMU';
 const bot = new TelegramBot(token, { polling: true });
 // const urlAmazon = 'https://0gb33gj0pj.execute-api.us-east-1.amazonaws.com/telegramBotGetMessage';
-const urlAmazon = 'https://2ib05hqbkh.execute-api.us-east-1.amazonaws.com/bot-order-report';
+// const urlAmazon = 'https://2ib05hqbkh.execute-api.us-east-1.amazonaws.com/bot-order-report';
+const urlAmazon = 'https://6b1ndyhayd.execute-api.us-east-1.amazonaws.com/bot-order-report';
 
 bot.on('message', async msg => {
   const {text, chat} = msg;
